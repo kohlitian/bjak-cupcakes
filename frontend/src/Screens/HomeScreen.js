5075525d111a31804e16c4b6f00173fa1b810db3
Li Tian <ben5green@outlook.com>
2021-05-08 00:32:55 +0800

    implemented loading and message box

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,15 +1,42 @@
-import React from 'react';
-import data from '../data';
+import React, { useEffect, useState } from 'react';
 import Product from '../components/Product';
+import Axios from 'axios';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 export default function HomeScreen() {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setLoading(true);
+                const { data } = await Axios.get('/api/products');
+                setLoading(false);
+                setProducts(data);
+            } catch (err) {
+                setError(err.message);
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, []);
+
     return (
         <div>
-            <div className="row center">
-                {data.products.map((product) => (
-                    <Product key={product._id} product={product} />
-                ))}
-            </div>
+            {loading ? (
+                <LoadingBox></LoadingBox>
+            ) : error ? (
+                <MessageBox variant="danger">{error}</MessageBox>
+            ) : (
+                <div className="row center">
+                    {products.map((product) => (
+                        <Product key={product._id} product={product} />
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
